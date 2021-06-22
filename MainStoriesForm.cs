d2b7e8fbc24cc271bb98f744ecce57f19072d188
ashsvis <ashsvis@gmail.com>
2021-06-22 13:43:00 +0300

    Добавьте файлы проекта.

---
+++ MainStoriesForm.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Stories
+{
+    public partial class MainStoriesForm : Form
+    {
+        public MainStoriesForm()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
